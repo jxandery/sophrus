e7759911543a1e0897d1850681798bc3f2bf15be
Jack Yeh <jack.yeh@gmail.com>
2019-08-17 21:28:52 -0500

    first pass at ticker model and specs

--- app/models/ticker.rb
+++ app/models/ticker.rb
@@ -1,2 +1,72 @@
 class Ticker < ApplicationRecord
+  # validates :instrument_id, :data, presence: true
+  # serialize :data
+  attr_reader :instrument, :data
+    # binding.pry
+
+  def initialize(data)
+    @instrument = instrument
+    @data = data
+  end
+
+  def valid_instrument?
+    instrument.symbol == pair_name
+  end
+
+  def errors
+    data[:error]
+  end
+
+  def pair_name
+    data[:result].keys.first.to_s
+  end
+
+  def tick_data
+    data[:result][symbol] }
+  end
+
+  def ask
+    # a = ask array(<price>, <whole lot volume>, <lot volume>),
+    tick_data[:a]
+  end
+
+  def bid
+    # b = bid array(<price>, <whole lot volume>, <lot volume>),
+    tick_data[:b]
+  end
+
+  def close
+    # c = last trade closed array(<price>, <lot volume>),
+    tick_data[:c]
+  end
+
+  def volume
+    # v = volume array(<today>, <last 24 hours>),
+    tick_data[:v]
+  end
+
+  def vol_wgt_avg
+    # p = volume weighted average price array(<today>, <last 24 hours>),
+    tick_data[:p]
+  end
+
+  def number_of_trades
+    # t = number of trades array(<today>, <last 24 hours>),
+    tick_data[:t]
+  end
+
+  def low
+    # l = low array(<today>, <last 24 hours>),
+    tick_data[:l]
+  end
+
+  def high
+    # h = high array(<today>, <last 24 hours>),
+    tick_data[:h]
+  end
+
+  def open
+    # o = today's opening price
+    tick_data[:o]
+  end
 end
--- spec/models/ticker_spec.rb
+++ spec/models/ticker_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Ticker, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#save' do
+    context 'when successful' do
+      let(:data) {
+        {
+          "error":[],
+          "result":{
+            "XXBTZUSD":{
+              "a":["8466.90000", "1", "1.000"],
+              "b":["8464.10000", "1", "1.000"],
+              "c":["8464.50000", "0.21218942"],
+              "v":["3171.04602409", "5795.97762632"],
+              "p":["8528.77032", "8611.98288"],
+              "t":[8319, 17457],
+              "l":["8350.00000", "8350.00000"],
+              "h":["8746.00000", "8841.30000"],
+              "o":"8740.00000"
+            }
+          }
+        }.symbolize_keys
+      }
+
+
+      let(:symbol) { 'XXBTZUSD'.to_sym }
+      let(:instrument) { Instrument.create(symbol: symbol) }
+
+      it 'creates ticker record' do
+        # let!(:ticker) { Ticker.create(instrument_id: instrument.id, data: data) }
+        ticker = Ticker.new(instrument, data)
+       binding.pry
+        ticker.save
+        expect(ticker.data).to eq(ticker_data[:result][symbol.to_sym])
+        expect(ticker.valid_instrument?).eq(true)
+        expect(ticker.pair_name).eq("XXBTZUSD")
+        expect(ticker.ask).eq(["8466.90000", "1", "1.000"])
+        expect(ticker.bid).to eq(["8464.10000", "1", "1.000"])
+        expect(ticker.close).to eq(["8464.50000", "0.21218942"])
+        expect(ticker.volume).to eq(["3171.04602409", "5795.97762632"])
+        expect(ticker.vol_wgt_avg).to eq(["8528.77032", "8611.98288"])
+        expect(ticker.number_of_trades).to eq([8319, 17457])
+        expect(ticker.low).to eq(["8350.00000", "8350.00000"])
+        expect(ticker.high).to eq(["8746.00000", "8841.30000"])
+        expect(ticker.open).to eq("8740.00000")
+      end
+
+      context 'when an error occurs' do
+        xit 'logs error message in slack' do
+
+        end
+      end
+    end
+  end
 end
