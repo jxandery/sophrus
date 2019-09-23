14d2e51b62af0577ea811759234860548c488cae
Jack Yeh <jack.yeh@gmail.com>
2019-09-22 21:30:32 -0500

    add time key helper methods

--- app/helpers/time_key_helper.rb
+++ app/helpers/time_key_helper.rb
@@ -1,5 +1,33 @@
 module TimeKeyHelper
-  def time_key
-    "#{Time.now.year}-#{Time.now.month}-#{Time.now.day}-#{Time.now.hour}"
+  def self.call
+    "#{year}-#{month}-#{day}-#{hour}"
+  end
+
+  def self.double_digit_format(num)
+    num.to_s.last(2).rjust(2, '0')
+  end
+
+  def self.year
+    double_digit_format(Time.now.year)
+  end
+
+  def self.month
+    double_digit_format(Time.now.month)
+  end
+
+  def self.day
+    double_digit_format(Time.now.day)
+  end
+
+  def self.hour
+    double_digit_format(Time.now.hour)
+  end
+
+  def self.min
+    double_digit_format(Time.now.min)
+  end
+
+  def self.sec
+    double_digit_format(Time.now.sec)
   end
 end
