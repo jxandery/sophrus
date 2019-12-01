243b098589653290b433be7dff90fea90f42c11b
Jack Yeh <jack.yeh@gmail.com>
2019-11-30 20:41:44 -0700

    add group hourly data

--- app/lib/group_hourly_data.rb
+++ app/lib/group_hourly_data.rb
@@ -4,20 +4,19 @@ class GroupHourlyData
 
   def initialize(ticker_data)
     @data = KrakenData.new(ticker_data)
-    @instrument = Instrument.find_by(symbol: data.find_symbol)
+    @instrument = Instrument.find_or_create_by!(symbol: data.find_symbol)
     @time_key = TimeKeyHelper.call
   end
 
   def call
     begin
-      ticker = Ticker.find_or_create_by!(instrument: instrument, time_key: time_key)
+      ticker = Ticker.find_or_create_by!(instrument_id: instrument.id, time_key: time_key)
 
       data.log_tick_data_error(time_key)
 
       ticker.data << data.time_stamped.to_json
       ticker.save!
     rescue => e
-      binding.pry
       SLACK.ping("Error: GroupHourlyData failed for #{instrument.symbol} at #{time_key}")
       Rails.logger.error("There was a GroupHourlyData error for #{instrument.symbol} at #{time_key}: #{e.inspect}")
     end
--- spec/lib/group_hourly_data_spec.rb
+++ spec/lib/group_hourly_data_spec.rb
@@ -3,42 +3,51 @@ require 'rails_helper'
 RSpec.describe GroupHourlyData do
   describe '.call' do
     let(:data) do
-      {
-        'error': ['error 1'],
-        'result': {
-          'XXBTZUSD': {
+      [
+        {
+          'error': ['error 1', 'error 2'],
+          'result': {
+            'XXBTZUSD': {
+              'a': ['8466.90000', '1', '1.000'],
+              'b': ['8464.10000', '1', '1.000'],
+              'c': ['8464.50000', '0.21218942'],
+              'v': ['3171.04602409', '5795.97762632'],
+              'p': ['8528.77032', '8611.98288'],
+              't': [8319, 17457],
+              'l': ['8350.00000', '8350.00000'],
+              'h': ['8746.00000', '8841.30000'],
+              'o': '8740.00000'
+            }
           }
         }
-      }
+      ]
     end
 
 
     let(:symbol) { 'XXBTZUSD'.to_sym }
     let!(:instrument) { Instrument.create(symbol: symbol) }
 
+    before do
+      Timecop.freeze('6/1/2015 01:23:45 MST')
+    end
+
     subject do
-      GroupHourlyData.new(data).call
+      GroupHourlyData.new(data.first).call
     end
 
     context 'when errors out' do
-      let(:data) do
-        {
-          'error': [],
-          'result': {
-            'XXBTZUSD': {
-            }
-          }
-        }
-      end
-
       it 'pings slack' do
-        # how do you get #save! to fail intentionally
-        allow(GroupHourlyData.new(data)).to receive(:call).and_return(StandardError)
-        allow(SLACK).to receive(:ping)
+        ticker_double = double(:ticker)
+        allow(Ticker).to receive(:find_or_create_by!).and_return(ticker_double)
+        allow(ticker_double).to receive(:data)
+        allow(ticker_double).to receive(:save!).and_return(StandardError)
+        allow(SLACK).to receive(:ping).exactly(3).times
 
         subject
 
-        expect(SLACK).to have_received(:ping).with("Error: GroupHourlyData.call failed for #{instrument.symbol}")
+        expect(SLACK).to have_received(:ping).with("Tick level error for 15-01-06-01. Error: error 1")
+        expect(SLACK).to have_received(:ping).with("Tick level error for 15-01-06-01. Error: error 2")
+        expect(SLACK).to have_received(:ping).with("Error: GroupHourlyData failed for #{instrument.symbol} at 15-01-06-01")
       end
 
     end
@@ -48,13 +57,6 @@ RSpec.describe GroupHourlyData do
         subject
 
         expect(Ticker.count).to eq(1)
-      end
-
-      it 'adds ticker data in JSON form' do
-        subject
-        ticker_data = Ticker.first.data.first
-
-        expect(JSON.parse(ticker_data).keys).to eq(['error', 'result', 'min_sec'])
       end
     end
   end
